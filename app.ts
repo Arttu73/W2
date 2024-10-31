4ede7d0cf5fa0501de0e6851bd6ed696c15fe432
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-10-31 11:23:55 +0200

    First

---
+++ app.ts
@@ -0,0 +1,89 @@
+console.log("Hello World!")
+
+type TVehicle = {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+let vehicle: TVehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+}
+
+console.log(vehicle)
+
+interface IVehicle {
+    model: string,
+    color: string,
+    year: number,
+    power: number
+}
+
+interface ICar extends IVehicle {
+    bodyType: string,
+    wheelCount: number
+}
+
+interface IBoat extends IVehicle {
+    draft: number
+}
+
+interface IPlane extends IVehicle {
+    wingSpan: number
+}
+
+let car: ICar = {
+    model: "Ford focus", 
+    color: "Green", 
+    year: 2016, 
+    power: 150, 
+    bodyType: "Hatchback", 
+    wheelCount: 4 
+}
+
+let plane: IPlane = {
+    model: "Boeing 777", 
+    color: "White", 
+    year: 2020, 
+    power: 170000, 
+    wingSpan: 65 
+}
+
+let boat: IBoat = {
+    model: "Bella", 
+    color: "Black", 
+    year: 2022, 
+    power: 100, 
+    draft: 0.42 
+}
+
+console.log(car)
+console.log(plane)
+console.log(boat)
+
+
+class VehicleService<T> {
+    items: T[] = []
+
+    add(item: T) {
+        this.items.push(item)
+    }     
+
+    list() {
+        console.log(this.items)
+    }
+}
+let cars = new VehicleService<ICar>
+
+let boats = new VehicleService<IBoat>
+
+cars.add(car)
+boats.add(boat)
+
+cars.list()
+
+boats.list()
---
+++ dist/app.js
@@ -0,0 +1,49 @@
+"use strict";
+console.log("Hello World!");
+let vehicle = {
+    model: "Boring generic vehicle",
+    color: "Red",
+    year: 1993,
+    power: 60
+};
+console.log(vehicle);
+let car = {
+    model: "Ford focus",
+    color: "Green",
+    year: 2016,
+    power: 150,
+    bodyType: "Hatchback",
+    wheelCount: 4
+};
+let plane = {
+    model: "Boeing 777",
+    color: "White",
+    year: 2020,
+    power: 170000,
+    wingSpan: 65
+};
+let boat = {
+    model: "Bella",
+    color: "Black",
+    year: 2022,
+    power: 100,
+    draft: 0.42
+};
+console.log(car);
+console.log(plane);
+console.log(boat);
+class VehicleService {
+    items = [];
+    add(item) {
+        this.items.push(item);
+    }
+    list() {
+        console.log(this.items);
+    }
+}
+let cars = new VehicleService;
+let boats = new VehicleService;
+cars.add(car);
+boats.add(boat);
+cars.list();
+boats.list();
